2fade65fc43a367d9f87638dca1505273cc243d5
Peter J. Philipp <pbug44@delphinusdns.org>
2023-08-09 06:00:28 +0200

    I use QEMU for riscv on windows server where I run an OpenBSD emulation instead of at home on my rpi4b, this saves costs in electricity.

---
+++ start-rv64.bat
@@ -0,0 +1,34 @@
+#  Copyright (c) 2023 Peter J. Philipp
+#  All rights reserved.
+# 
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#  1. Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#  3. The name of the author may not be used to endorse or promote products
+#     derived from this software without specific prior written permission
+# 
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+#  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+#  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+#  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+#  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# This script is my current config for running qemu (riscv) on Windows server
+# The version of windows server I use is 2019.
+#
+# I needed to install the openvpn program for the windows tap interfaces
+# this was gotten from https://openvpn.net
+#
+cd C:\Users\pjp\Desktop
+
+qemu-system-riscv64w.lnk -smp 2,sockets=2,maxcpus=2 -M virt -m 2G -bios fw_jump.bin -kernel u-boot.bin -device virtio-net-device,netdev=mytap0 -netdev tap,id=mytap0,ifname=TAP0  -device virtio-blk-device,drive=sd -drive file="C:\Users\pjp\new.img",if=none,id=sd -device virtio-blk-device,drive=hd -drive file="\Users\pjp\backup.img",if=none,id=hd
